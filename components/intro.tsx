aa838950fe86e68848782a3ea4a403e573031b91
vijay <60967175+vijay10011@users.noreply.github.com>
2022-12-15 15:32:55 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
         techfuture
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using
+        
         <a
           
           className="underline hover:text-success duration-200 transition-colors"
